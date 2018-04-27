d579f620b280f1f5ca4c6fd7de27b8d5ab82f41d
Damilare <damilare.durojaye@gmail.com>
2018-04-27 05:00:49 +0100

    Initial commit to end of tutorial

---
+++ src/components/Search.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="search">
+        <h2>{{ msg }}</h2>
+        <form v-on:submit.prevent="getResult(query)">
+            <input type="text" placeholder="Type in your search term" v-model="query"/>
+        </form>
+        <div class="results" v-if="results">
+            <div v-for="result in results">
+                <img v-bind:src="result.links[0].href" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        name: 'search',
+        data () {
+            return {
+                msg: 'Type In Your Search Term',
+                query: '',
+                results: ''
+            }
+        },
+        methods: {
+            getResult(query) {
+                //axios.get('http://www.reddit.com/r/pics.json')
+                axios.get(`https://images-api.nasa.gov/search?q=${query}&media_type=image`)
+                    .then(response => {
+                        console.log(response.data.collection.items);
+                        this.results = response.data.collection.items
+                    });
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    .results img {
+        height: 300px;
+        margin: 10px;
+    }
+</style>
